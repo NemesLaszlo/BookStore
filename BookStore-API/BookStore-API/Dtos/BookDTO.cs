2cb82848a3f3545cc9aaee2997ee37ea5a9ed690
NemesLaszlo <wow.laszlo@gmail.com>
2021-02-26 21:10:12 +0100

    Book Create and Edit (Update) Forms with functionality (picture upload too)

--- BookStore-API/BookStore-API/Dtos/BookDTO.cs
+++ BookStore-API/BookStore-API/Dtos/BookDTO.cs
@@ -52,6 +52,8 @@ namespace BookStore_API.Dtos
         [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+        [Required]
+        public string Isbn { get; set; }
         [StringLength(500)]
         public string Summary { get; set; }
         public string Image { get; set; }

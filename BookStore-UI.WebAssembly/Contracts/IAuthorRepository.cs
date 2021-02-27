a269e7f07b942a5c117d72405b8f7b371df2d4fb
NemesLaszlo <wow.laszlo@gmail.com>
2021-02-27 14:10:01 +0100

    Init and Refactor UI to WebAssembly App

---
+++ BookStore-UI.WebAssembly/Contracts/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore_UI.WebAssembly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.WebAssembly.Contracts
+{
+    public interface IAuthorRepository : IBaseRepository<Author>
+    {
+
+    }
+}
---
+++ BookStore-UI.WebAssembly/Service/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using Blazored.LocalStorage;
+using BookStore_UI.WebAssembly.Contracts;
+using BookStore_UI.WebAssembly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.WebAssembly.Service
+{
+    public class BookRepository : BaseRepository<Book>, IBookRepository
+    {
+
+        public BookRepository(HttpClient client, ILocalStorageService localStorage) : base(client, localStorage)
+        {
+
+        }
+    }
+}
